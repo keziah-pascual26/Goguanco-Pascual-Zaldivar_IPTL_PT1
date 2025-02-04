27c9528bb6b9c3ce2536c0e35c0dba6fb5c1bb42
Jana Zaldivar <janazaldivar03@gmail.com>
2025-02-05 01:08:20 +0800

    fixed image preview

--- script.js
+++ script.js
@@ -439,6 +439,17 @@ async function addStories() {
             previewElement = document.createElement('img');
             previewElement.src = URL.createObjectURL(file); // Initially set to the original file
             previewElement.style.maxWidth = '100%';
+
+                        // If cropping is enabled and we have cropped image data, use that for preview
+                        if (croppedImageData) {
+                            previewElement.src = croppedImageData;  // Use cropped image for preview
+                            console.log("Displaying cropped image in preview");
+                        } else {
+                            previewElement.src = URL.createObjectURL(file);  // Use original image if no cropping
+                        }
+            
+                        // Apply rotation and resize for preview
+                        previewElement.style.transform = `rotate(${rotationAngle}deg) scale(${resizeFactor})`;
         }
         else if (fileType === 'video') {
             previewElement = document.createElement('video');
